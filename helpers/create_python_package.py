9ae94f6c0c1c869d409532e2bab5740626daeacc
Dominik Gresch <greschd@ethz.ch>
2016-03-30 00:52:12 +0200

    added a script that creates a python package from the template

--- helpers/create_python_package.py
+++ helpers/create_python_package.py
@@ -7,9 +7,28 @@
 
 import os
 import sys
+import shutil
 
 def create_package(full_name, import_name):
     existing_modules = os.listdir('../modules')
+    for name in [full_name, import_name]:
+        if name in existing_modules:
+            raise ValueError('A module with name {} exists already'.format(name))
+
+    module_dest = '../modules/' + full_name
+    shutil.copytree('./python_package_template', module_dest)
+    shutil.move(
+        os.path.join(module_dest, 'fsc', 'pkg_tpl'),
+        os.path.join(module_dest, 'fsc', import_name)
+    )
+
+    for path, _, filenames in os.walk(module_dest):
+        for filename in filenames:
+            abspath = os.path.join(path, filename)
+            with open(abspath, 'r') as f:
+                text = f.read()
+            with open(abspath, 'w') as f:
+                f.write(text.replace('{PKGNAME}', import_name))
 
 def main():
     if len(sys.argv) != 3:
