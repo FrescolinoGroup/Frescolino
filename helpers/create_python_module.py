be1a728bb21ac006576c53f4fe76e83a415b82e1
Dominik Gresch <greschd@ethz.ch>
2016-03-30 13:19:47 +0200

    removed single-file template, added symlink for version

--- helpers/create_python_module.py
+++ helpers/create_python_module.py
@@ -10,25 +10,18 @@ import sys
 import shutil
 import argparse
 
-def create_module(full_name, import_name, single_file=True):
+def create_module(full_name, import_name):
     existing_modules = os.listdir('../modules')
     for name in [full_name, import_name]:
         if name in existing_modules:
             raise ValueError('A fsc module with name {} exists already'.format(name))
 
     mod_dest = '../modules/' + full_name
-    if single_file:
-        shutil.copytree('python_module_template', mod_dest)
-        shutil.move(
-            os.path.join(mod_dest, 'fsc', 'mod_tpl.py'),
-            os.path.join(mod_dest, 'fsc', import_name + '.py')
-        )
-    else:
-        shutil.copytree('python_package_template', mod_dest)
-        shutil.move(
-            os.path.join(mod_dest, 'fsc', 'pkg_tpl'),
-            os.path.join(mod_dest, 'fsc', import_name)
-        )
+    shutil.copytree('python_package_template', mod_dest)
+    shutil.move(
+        os.path.join(mod_dest, 'fsc', 'pkg_tpl'),
+        os.path.join(mod_dest, 'fsc', import_name)
+    )
 
     shutil.copytree('python_doc_template', os.path.join(mod_dest, 'doc'))
 
@@ -42,20 +35,9 @@ def create_module(full_name, import_name, single_file=True):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--module', dest='module', action='store_true', help='Create a single-file module.')
-    parser.add_argument('-p', '--package', dest='package', action='store_true', help='Create a package directory.')
 
     parser.add_argument(dest='full_name', type=str)
     parser.add_argument(dest='import_name', type=str)
     args = parser.parse_args(sys.argv[1:])
 
-    if args.module and args.package:
-        print('Cannot create both a module and a package. Choose either -m or -p.')
-    elif args.module:
-        print('Creating a single-file module.')
-        create_module(args.full_name, args.import_name, single_file=True)
-    elif args.package:
-        print('Creating a package directory.')
-        create_module(args.full_name, args.import_name, single_file=False)
-    else:
-        print('Choose between creating single-file module [-m] and a package directory [-p] by setting the appropriate flag.')
+    create_module(args.full_name, args.import_name)
--- helpers/python_package_template/fsc/pkg_tpl/_version.py
+++ helpers/python_package_template/fsc/pkg_tpl/_version.py
@@ -7,5 +7,5 @@
 
 import os
 
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../version.txt'), 'r') as f:
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'version.txt'), 'r') as f:
     __version__ = f.read().strip()
